5dac22d9beddc6e376fc1a018e4a6da8a9406c6f
Lunesombreelite <94185038+Lunesombreelite@users.noreply.github.com>
2023-03-20 18:20:01 +0100

    Create SupprPersonnel.php

---
+++ admin/SupprPersonnel.php
@@ -0,0 +1,12 @@
+<?php
+    include_once('../include.php');
+    error_reporting(E_ALL);
+    ini_set("display_errors", 1);
+
+    if(isset($_GET["id"])AND is_numeric($_GET["id"]))
+    {
+        $id = htmlentities($_GET['id']);
+        $sql= $DB->exec("DELETE FROM personnel where id = " . $id . ";");
+    }
+    header("Location : ajout_service.php");
+?>
