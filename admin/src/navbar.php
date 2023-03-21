4f83629a0acca7e2c44b7139fe5b6f43db5c04c4
su apt install kuma <92596486+JustArthur@users.noreply.github.com>
2023-03-21 13:45:24 +0100

    Update navbar.php

--- admin/src/navbar.php
+++ admin/src/navbar.php
@@ -1,36 +1,25 @@
 <nav>
     <ul class="list">
-        <li><a href="panel">Accueil</a></li>
+        <li><a href="panel.php">Accueil</a></li>
 
         <?php if($_SESSION['utilisateur'][3] == 1) { ?>
-            <li><a href="voir_preadmission">Pré-admission</a>
+            <li><a href="voir_preadmission.php">Pré-admission</a>
                 <ul class="dropdown">
-                    <li><a href="num_secu_creer">Créer une pré-admision</a></li>
-                    <li><a href="num_secu_modif">Modifier une pré-admision</a></li>
-                    <li><a href="num_secu_supp">Supprimer une pré-admision</a></li>
+                    <li><a href="num_secu_creer.php">Créer une pré-admision</a></li>
+                    <li><a href="num_secu_modif.php">Modifier une pré-admision</a></li>
+                    <li><a href="num_secu_supp.php">Supprimer une pré-admision</a></li>
                 </ul>
             </li>
+
+            <li><a href="pdf_semaine.php">Créer un PDF</a></li>
         <?php } ?>
 
         <?php if($_SESSION['utilisateur'][3] == 2) { ?>
-            <li><a href="voir_personnel">Personnel</a>
-                <ul class="dropdown">
-                    <li><a href="#">Ajouter un personnel</a></li>
-                    <li><a href="#">Modifier un personnel</a></li>
-                    <li><a href="#">Supprimer un personnel</a></li>
-                </ul>
-            </li>
-
-            <li><a href="voir_service">Service</a>
-                <ul class="dropdown">
-                    <li><a href="#">Ajouter un service</a></li>
-                    <li><a href="#">Modifier un service</a></li>
-                    <li><a href="#">Supprimer un service</a></li>
-                </ul>
-            </li>
+            <li><a href="ajout_service.php">Personels</a></li>
+            <li><a href="ajout_service.php">Services</a></li>
         <?php } ?>
 
-        <li><a href="statistique">Statistiques</a></li>
+        <li><a href="statistique.php">Statistiques</a></li>
         <li><a href="src/deconnexion.php">Se déconnecter</a></li>
     </ul>
 </nav>
