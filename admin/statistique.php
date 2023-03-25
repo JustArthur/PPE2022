b5592d284740f768f22bda5973d03c5b16675cf1
Lunesombreelite <94185038+Lunesombreelite@users.noreply.github.com>
2023-03-25 22:48:48 +0100

    Update statistique.php

--- admin/statistique.php
+++ admin/statistique.php
@@ -10,57 +10,65 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <title>Stastistique</title>
     <link rel="stylesheet" href="../style/statistique.css">
     <link rel="stylesheet" href="../style/navBar.css">
-
-    <title>Stastistique</title>
 </head>
-    <?php
-        require_once('src/navbar.php');
-    ?>
-
-    <main>
+<body>
+<div>
     <div>
     <canvas id="myChartPersonelle"></canvas>
     <canvas id="myChartOperation"></canvas>
-    <canvas id="myChartOperationDate"></canvas>
     </div>
     <?php
+
       $data1 = $DB->prepare('SELECT id,service From personnel');
       $data1->execute();
       $salaries = $data1->fetchall();
       foreach($salaries as $salarie){
-      
       }
+
       $data2 = $DB->prepare('SELECT id FROM operations');
       $data2->execute();
       $operations = $data2->fetchall();
       foreach($operations as $operation){
-
       }
-      $date3 = $DB->prepare('SELECT from operations');
-      $date3->execute();
-      $oparationdates = $date3->fetchall();
-      foreach($oparationdates as $oparationdate){
 
+      $data3 = $DB->prepare('SELECT count(nomOperation) FROM operations where nomOperation = "Hospitalisation"');
+      $data3->execute();
+      $hospitalisations = $data3->fetchall();
+      foreach($hospitalisations as $hospitalisation){
+      }
+
+      $data4 = $DB->prepare('SELECT count(nomOperation) FROM operations where nomOperation = "Ambulatoire chirugie"');
+      $data4->execute();
+      $Ambulatoires = $data4->fetchall();
+      foreach($Ambulatoires as $Ambulatoire){
+      }
+
+      $date5 = $DB->prepare('SELECT count(YEAR(dateOperation)) FROM operations WHERE YEAR(dateOperation)=2023');
+      $date5->execute();
+      $dates2023 = $date5->fetchall();
+      foreach($dates2023 as $date2023){
+      }
+
+      $date6 = $DB->prepare('SELECT count(YEAR(dateOperation)) FROM operations WHERE YEAR(dateOperation)=2022');
+      $date6->execute();
+      $dates2022 = $date6->fetchall();
+      foreach($dates2022 as $date2022){
       }
     ?>
-    </main>
-    
-
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>    
     <script>
     const ctxPersonnelle = document.getElementById('myChartPersonelle');
     const ctxOperation = document.getElementById('myChartOperation');
-    const ctxOperationDate = document.getElementById('myChartOperationDate');
+      // premier graphique //
     new Chart(ctxPersonnelle, {
       type: 'bar',
       data: {
-        labels: ['personnel '],
+        labels: ['personnel'],
         datasets: [{
           data: [<?php echo $salarie['id']; ?>],
-    
         }]
       },    
       options: {
@@ -71,12 +79,13 @@
         }
       }
     });
+      // deuxieme graphique // 
     new Chart(ctxOperation,{
         type: 'bar',
         data: {
             labels: ['operation'],
             datasets:[{
-              data:[<?php echo $operation['id']?>],
+              data:[<?php echo $operation['id'];?>],
             }]
         },
         options: {
@@ -88,7 +97,106 @@
       }
     });
     </script>
-<body>
-    
-</body>
+    <select name="nomOperation">
+    <?php
+      $reponse = $DB->query('SELECT   DISTINCT nomOperation FROM operations');
+      while($donnees = $reponse->fetch()){
+    ?>
+      <option value="<?php echo $donnees['nomOperation'];?>"><?php echo $donnees['nomOperation']?></option>
+        
+    <?php }?>
+    </select>
+    <div>
+      <canvas id="myCharthospitalisation"></canvas>
+      <canvas id="myChartAmbulatoire"></canvas>
+    </div>
+    <script>
+    const ctxhospitalisation = document.getElementById('myCharthospitalisation');
+    const ctxhAmbulatoire = document.getElementById('myChartAmbulatoire');
+    // troisieme graphique // 
+     new Chart(ctxhospitalisation,{
+        type: 'bar',
+        data: {
+            labels: ['hospitalisation'],
+            datasets:[{
+              data:[<?php echo $hospitalisation['count(nomOperation)'];?>],
+            }]
+        },
+        options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+    // quatrieme graphique //
+    new Chart(ctxhAmbulatoire,{
+        type: 'bar',
+        data: {
+            labels: ['Ambulatoire chirugie'],
+            datasets:[{
+              data:[<?php echo $Ambulatoire['count(nomOperation)'];?>],
+            }]
+        },
+        options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+    </script>
+     <select name="dateOperation">
+    <?php
+      $reponse = $DB->query('SELECT  DISTINCT YEAR(dateOperation) FROM operations');
+      while($donnees = $reponse->fetch()){
+    ?>
+      <option value="<?php echo $donnees['YEAR(dateOperation)']; ?>"><?php echo $donnees['YEAR(dateOperation)']?></option>
+    <?php }?>
+    </select>
+    <div>
+      <canvas id="myChartDate2023"></canvas>
+      <canvas id="myChartDate2022"></canvas>
+    </div>
+    <script>
+    const ctxDate2023 = document.getElementById('myChartDate2023');
+    const ctxhDate2022 = document.getElementById('myChartDate2022');
+    // cinquieme graphique // 
+     new Chart(ctxDate2023,{
+        type: 'bar',
+        data: {
+            labels: ['date 2023'],
+            datasets:[{
+              data:[<?php echo $date2023['count(YEAR(dateOperation))'];?>],
+            }]
+        },
+        options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+    // sixieme  graphique //
+    new Chart(ctxhDate2022,{
+        type: 'bar',
+        data: {
+            labels: ['date 2022'],
+            datasets:[{
+              data:[<?php echo $date2022['count(YEAR(dateOperation))'];?>],
+            }]
+        },
+        options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+    </script>
+  </body> 
 </html>
